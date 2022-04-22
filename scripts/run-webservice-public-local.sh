76e5f57a3839b8749d817498c534b0b0f0dd0394
Adrian Wilke <adibaba@users.noreply.github.com>
2022-04-22 08:29:51 +0200

    es python to v8

--- scripts/run-webservice-public-local.sh
+++ scripts/run-webservice-public-local.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Starting Elasticsearch"
-sudo systemctl start elasticsearch.service
+#echo "Starting Elasticsearch"
+#sudo systemctl start elasticsearch.service
 
 echo "Available embeddings"
 conda env list
 
 echo "Starting webservice public"
 eval "$(conda shell.bash hook)" # https://stackoverflow.com/a/56155771
-conda activate embeddings
+conda activate embeddings8
 export FLASK_APP=webservice_public
 export FLASK_RUN_PORT=1337
 export FLASK_DEBUG=True
