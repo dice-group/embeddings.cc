1050e1d76e541b7fc9bfa9ddd316a1006d512d19
Adrian <adibaba@users.noreply.github.com>
2023-01-26 11:57:31 +0100

    updated es-index backup script

--- scripts/backup-es-index.py
+++ scripts/backup-es-index.py
@@ -8,21 +8,19 @@
 
 import sys
 from elasticsearch import Elasticsearch
-import bz2
-
-# Do not show warning for self-signed certificates
 import urllib3
-urllib3.disable_warnings()
+urllib3.disable_warnings() # Do not show warning for self-signed certificates
+sys.path.insert(1, '../api/serialization/')
+from file_writer import FileWriter
+
 
 # Configuration
 host = "https://embeddings.cs.upb.de:9200"
 user = "elastic"
-size = 10*1000
+size = 10 * 1000
 keep_context = "60s"
-use_bz2 = True
-index = "dbpedia_en_fr_15k_procrustes"
-#index = "dbpedia_en_fr_100k_procrustes_v2"
-#index = "dbpedia_wikidata_full"
+file_format = FileWriter.FORMAT_BZIP2  # FORMAT_BZIP2 or FORMAT_TEXT
+
 
 # Get password form CLI
 if len(sys.argv) > 1:
@@ -31,26 +29,25 @@ else:
     print('Please provide the Elasticsearch user password')
     sys.exit(1)
 
-# Open file
-if use_bz2:
-    filename_uris = index + '.uris.txt.bz2'
-    f_uris = bz2.open(filename_uris, mode='wt', compresslevel=9, encoding=None, errors=None, newline=None)
-    filename_emb = index + '.embeddings.txt.bz2'
-    f_emb = bz2.open(filename_emb, mode='wt', compresslevel=9, encoding=None, errors=None, newline=None)
+# Get index form CLI
+if len(sys.argv) > 2:
+    index = sys.argv[2]
 else:
-    filename_uris = index + '.uris.txt'
-    f_uris = open(filename_uris, 'w')
-    filename_emb = index + '.embeddings.txt'
-    f_emb = open(filename_emb, 'w')
-print('Writing to:', filename_uris, filename_emb)
+    print('Please provide the Elasticsearch index to backup')
+    sys.exit(1)
 
-def handle_respose(response):
+
+# Writing
+writer = FileWriter(id=index, format=file_format)
+writer.open()
+
+def handle_respose(response, file_writer):
     print('.', end='', flush=True)
     for doc in response['hits']['hits']:
-        f_emb.write(",".join(str(emb) for emb in doc['_source']['embeddings']) + '\n')
-        f_uris.write(doc['_source']['entity'] + '\n')
+        writer.add(doc['_source']['entity'], doc['_source']['embeddings'])
 
-# Default query
+
+# Default query and first request
 client = Elasticsearch(host, http_compress=True, verify_certs=False, basic_auth=(user, password))
 match_all = {
     "size": size,
@@ -59,22 +56,21 @@ match_all = {
     }
 }
 response = client.search(
-    index = index,
-    body = match_all,
-    scroll = keep_context
+    index=index,
+    body=match_all,
+    scroll=keep_context
 )
-handle_respose(response)
+handle_respose(response, writer)
 scroll_id = response['_scroll_id']
 
 # Request until no further results available
 while len(response['hits']['hits']):
     response = client.scroll(
-        scroll_id = scroll_id,
-        scroll = keep_context
+        scroll_id=scroll_id,
+        scroll=keep_context
     )
-    handle_respose(response)
+    handle_respose(response, writer)
     scroll_id = response['_scroll_id']
 
-# Finally close file
-f_uris.close()
-f_emb.close()
+# Finally close files
+writer.close()
