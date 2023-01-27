370dd82b2073f14f834e71fa9511ad7aefacac15
Adrian <adibaba@users.noreply.github.com>
2023-01-27 11:17:13 +0100

    test runs for faiss

--- dev/faiss_indexer.py
+++ dev/faiss_indexer.py
@@ -4,49 +4,67 @@ sys.path.insert(1, '../api/serialization/')
 from file_reader import FileReader
 import pickle
 import faiss
-from faiss_api import faiss_api
+sys.path.insert(1, '../faiss_api/')
+import faiss_api
+
 # Download the data here:
 # https://hobbitdata.informatik.uni-leipzig.de/embeddings_cc/data/dbpedia_en_fr_15k_procrustes.uris.txt.bz2
 # https://hobbitdata.informatik.uni-leipzig.de/embeddings_cc/data/dbpedia_en_fr_15k_procrustes.embeddings.txt.bz2
 
 # Set the local data path:
-#data_path = '/home/wilke/Data/embeddings_cc/data/'
-data_path = '../datatoindex/'
-# Load data (30,000 pairs):
-data = []
+data_path = '/home/wilke/Data/embeddings_cc/data/'
+#data_path = '../datatoindex/'
 
-for item in FileReader(embeddings_file=data_path+'dbpedia_en_fr_15k_procrustes.embeddings.txt.bz2', uri_file=data_path+'dbpedia_en_fr_15k_procrustes.uris.txt.bz2', format='bzip2'):
+# Set data files:
+if True:
+    file_embs = 'dbpedia_en_fr_15k_procrustes.embeddings.txt.bz2' # (30,000 pairs)
+    file_uris = 'dbpedia_en_fr_15k_procrustes.uris.txt.bz2'
+else:
+    file_embs = 'example.embeddings.txt.bz2' # (4 pairs)
+    file_uris = 'example.uris.txt.bz2'
+
+# Set approximation (or exact):
+hnsw_appr = True
+
+# Load data:
+data = []
+for item in FileReader(embeddings_file=data_path+file_embs, uri_file=data_path+file_embs, format='bzip2'):
     data.append((item[0], item[1]))
-print('Types:          ', type(data[0]), type(data[0][0]), type(data[0][1]), type(data[0][1][0]))
-print('Dimensions:     ', len(data[-1][1]))
-print('First item:     ', data[0])
-print('Last item:      ', data[-1])
+if False:
+    print('Types:          ', type(data[0]), type(data[0][0]), type(data[0][1]), type(data[0][1][0]))
+    print('Dimensions:     ', len(data[-1][1]))
+    print('First item:     ', data[0])
+    print('Last item:      ', data[-1])
 print('Number of items:', len(data))
 
-index_name="faiss_dense_appr"
-id_to_index_name=index_name+"_dict.sav"
-
+# Compute index
 print("start indexing")
 max_id = 0
 x_dim = len(data)
 y_dim = len(data[0][1])
-#index = faiss_api.DenseFlatIndexer(vector_sz=y_dim)
-#option2 two HNSW index -> approximate index
-index = faiss_api.DenseHNSWFlatIndexer(vector_sz=y_dim)
+if not hnsw_appr:
+    index = faiss_api.DenseFlatIndex(vector_sz=y_dim)
+else:
+    #option2 two HNSW index -> approximate index
+    index = faiss_api.DenseHNSWFlatIndex(vector_sz=y_dim)
 vectors = numpy.zeros((x_dim, y_dim), dtype=numpy.float32)
 for sample in data:
     vectors[max_id] = numpy.asarray(sample[1])
     index.index_id_to_db_id.update({max_id: sample[0]})
     max_id = max_id + 1
-
-
 index.index_data(vectors)
+
+# Save
+if hnsw_appr:
+    index_name="faiss_dense_appr"
+else:
+    index_name="faiss_dense"
+id_to_index_name=index_name+"_dict.sav"
 
 print(index.index.ntotal)
 pickle.dump(index.index_id_to_db_id, open(id_to_index_name, 'wb'))
 faiss.write_index(index.index, index_name)
 print("finished")
-
 
 
 # TODO: Create a Faiss index containing the data
--- dev/test.py
+++ dev/test.py
@@ -1,20 +1,40 @@
-from faiss_api import faiss_api
 import sys
+sys.path.insert(1, '../faiss_api/')
+import faiss_api
 sys.path.insert(1, '../api/serialization/')
 from file_reader import FileReader
 
-data_path = '../datatoindex/'
-# Load data (30,000 pairs):
+# Set the local data path:
+data_path = '/home/wilke/Data/embeddings_cc/data/'
+#data_path = '../datatoindex/'
+
+# Set data files:
+if True:
+    file_embs = 'dbpedia_en_fr_15k_procrustes.embeddings.txt.bz2' # (30,000 pairs)
+    file_uris = 'dbpedia_en_fr_15k_procrustes.uris.txt.bz2'
+else:
+    file_embs = 'example.embeddings.txt.bz2' # (4 pairs)
+    file_uris = 'example.uris.txt.bz2'
+
+# Set approximation (or exact):
+hnsw_appr = False
+    
+# Load data:
 data = []
-for item in FileReader(embeddings_file=data_path+'dbpedia_en_fr_15k_procrustes.embeddings.txt.bz2', uri_file=data_path+'dbpedia_en_fr_15k_procrustes.uris.txt.bz2', format='bzip2'):
+for item in FileReader(embeddings_file=data_path+file_embs, uri_file=data_path+file_embs, format='bzip2'):
     data.append((item[0], item[1]))
-print('Types:          ', type(data[0]), type(data[0][0]), type(data[0][1]), type(data[0][1][0]))
-print('Dimensions:     ', len(data[-1][1]))
-print('First item:     ', data[0])
-print('Last item:      ', data[-1])
+if False:
+    print('Types:          ', type(data[0]), type(data[0][0]), type(data[0][1]), type(data[0][1][0]))
+    print('Dimensions:     ', len(data[-1][1]))
+    print('First item:     ', data[0])
+    print('Last item:      ', data[-1])
 print('Number of items:', len(data))
 
-search_index=faiss_api.load_Exact_index("faiss_dense","faiss_dense_dict.sav")
-#sv=data[0][1]
-#res=search_index.search([data[0][1]],5)
+if hnsw_appr:
+    search_index=faiss_api.load_HNSW_index("faiss_dense_appr","faiss_dense_appr_dict.sav", 300)
+else:
+    search_index=faiss_api.load_Exact_index("faiss_dense","faiss_dense_dict.sav")
+
+print(search_index.search([data[0][1]],5))
+print()
 print(search_index.getVectorForId(2))
