c23c30382400d8f9ab85cb004e254260ca978d70
Jean-KOUAGOU <jeank@aims.ac.za>
2024-10-11 15:22:00 +0200

    merge train.sh into install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash -e
 eval "$(conda shell.bash hook)"
 echo "before calling source: $PATH"
+git clone https://github.com/dice-group/embeddings.cc.git && cd embeddings.cc
 conda create -n embcc python=3.10 --y
 conda activate embcc
 echo "after calling source: $PATH"
@@ -11,3 +12,5 @@ wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.3.3-li
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.3.3-linux-x86_64.tar.gz.sha512
 shasum -a 512 -c elasticsearch-8.3.3-linux-x86_64.tar.gz.sha512 
 tar -xzf elasticsearch-8.3.3-linux-x86_64.tar.gz
+wget https://files.dice-research.org/datasets/dice-embeddings/KGs.zip --no-check-certificate && unzip KGs.zip && rm -rf KGs.zip
+dicee --dataset_dir KGs/UMLS --model DeCaL --num_epochs 1 --batch_size 512 --path_to_store_single_run ./embeddings --save_embeddings_as_csv --eval_model "None" --embedding_dim 64
